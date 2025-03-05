5780ed2e3ee2495e46b5a78e61318046749f88a9
geatz Eatz <eatz6677@gmail.com>
2025-03-05 18:49:55 +0000

    more terraform

--- s3/iac/terraform/main.tf
+++ s3/iac/terraform/main.tf
@@ -7,14 +7,6 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "default{
-}
-
-resource "aws_s3_object" "object"{
-  bucket = resource.aws_s3_bucket.default
-  key    = "myfile.txt"
-  source = "myfile.txt"
-}
 provider "aws" {
   # Configuration options
 }
